f749797c292bc1dff8a4441d23753cc4d2847541
Jeroen Seeuws <jeroen.seeuws@student.howest.be>
2020-10-05 11:17:46 +0200

    Modify contact page

--- RazorPagesDemo/Pages/Contact.cshtml.cs
+++ RazorPagesDemo/Pages/Contact.cshtml.cs
@@ -7,10 +7,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPagesDemo.Pages
 {
+    [BindProperties]
     public class ContactModel : PageModel
     {
+        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public string Feedback { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            Feedback = $"Thank you {Name} for your message. We will reply as soon as possible to {Email}";
         }
     }
 }
